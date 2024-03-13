2eb8b7cbf103d51972da6be82a8de0e4c1a73c78
LuisMont13 <90803215+LuisMont13@users.noreply.github.com>
2024-03-12 21:10:05 -0700

    Revert "Initial setup, needs styling for multiple pages"
    This reverts commit 8e2941afe825495ed9366fd7f47ec30b55e284b3.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import NBA from "./components/pages/nba";
-import MLB from "./components/pages/mlb";
-import EPL from "./components/pages/epl";
 
-  function App() {
-    let component
-    switch(window.location.pathname){
-      case "/":
-        component = <App />
-        break
-      case  "/nba":
-        component = <NBA />
-        break
-      case "/mlb":
-        component = <MLB />
-        break
-      case "/epl":
-        component = <EPL />
-        break
-    }
-    return (
-    <>
-      <Navbar />
-      {component}
-    </>
-    )
+  export default function App() {
+  return (
+  <div className="App">
+    <Navbar/>  
+  </div>
+
+
+  );
 }
-export default App
