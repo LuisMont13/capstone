35502c376df34e7b3ee3a6b3c8d7e2cb0780b6d5
LuisMont13 <90803215+LuisMont13@users.noreply.github.com>
2024-03-12 21:45:42 -0700

    fixed infinite loop for compilation

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import NBA from "./components/pages/nba";
+import MLB from "./components/pages/mlb";
+import EPL from "./components/pages/epl";
 
-  export default function App() {
+function App(){
+  let component
+  switch(window.location.pathname){
+    case "/":
+      component = <index />
+      break
+    case "/nba":
+      component = <NBA />
+      break
+    case "/mlb":
+      component = <MLB />
+      break
+    case "/epl":
+      component = <EPL />
+      break
+      
+  }
+
   return (
-  <div className="App">
-    <Navbar/>  
-  </div>
+    <>
+      <Navbar />
+      {component}
+    </>
+  )
 
-
-  );
 }
+export default App;
---
+++ client/src/components/pages/nba.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NBA = () => {
+    return(
+        <div>
+            <h1>This is where you can find NBA predictions.
+            </h1>
+        </div>
+    )
+}
+export default NBA
